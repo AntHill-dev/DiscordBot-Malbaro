dfaa2850a75abdd850ebee878efe762e7e74b027
Ivan Kovach <eeiprex@gmail.com>
2023-10-02 22:29:40 +0300

    [add:docs] docstring in `example.py` in "cogs/user"
    Also add func of registration - "setup"

--- bot/cogs/user/example.py
+++ bot/cogs/user/example.py
@@ -4,12 +4,28 @@ from discord.ext import commands
 from bot.misc.types import AppContext, BotType
 
 
-class ExampleCommands(commands.Cog):
-    bot: BotType
+class ExampleCommand(commands.Cog):
+    """The cog class of example commands.
 
-    def __init__(self, bot: BotType) -> None:
-        self.bot = bot
+    Methods: (sub-commands)
+        hello(self, cts: AppContext) -> None:
+            User greeting command.
+    """
 
     @discord.command(name="hello", description="Приветствие!")
     async def hello(self, ctx: AppContext) -> None:
+        """Hello user slash command.
+
+        Args:
+            ctx: All context when calling a command.
+        """
         await ctx.respond(f"Hello, {ctx.author.name}")
+
+
+def setup(bot: BotType) -> None:
+    """Load function cog ExampleCommand for bot.
+
+    Args:
+        bot: Bot for which cog will be loaded.
+    """
+    bot.add_cog(ExampleCommand())
