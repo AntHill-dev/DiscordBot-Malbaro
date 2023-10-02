62e31b74ae4bd10576574f4685e038fc57630531
Ivan Kovach <eeiprex@gmail.com>
2023-10-02 22:23:50 +0300

    [chore#env] Add `CommandPrefixDiscordBot` class
    Also add docstring for "APIKeyDiscordBot", "CommandPrefixDiscordBot"

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -4,15 +4,16 @@ from typing import Final
 
 from dotenv import load_dotenv
 
-from bot.misc.utils import SingletonABC
-
 load_dotenv()
 
 
-class APIKeyDiscordBot(SingletonABC):
-    TOKEN: Final[str] = field(default=os.environ.get("TOKEN", "TOKEN not found"))
+class APIKeyDiscordBot:
+    """Storage for the bot's secret token."""
+
+    TOKEN: Final[str] = field(default=os.environ.get("TOKEN", "!TOKEN"))
 
 
-class CommandPrefixDiscordBot(SingletonABC):
-    COMMAND_PREFIX: Final[str] = field(default=os.environ.get("COMMAND_PREFIX", "COMMAND_PREFIX not found"))
+class CommandPrefixDiscordBot:
+    """Storage for the bot's command prefix."""
 
+    COMMAND_PREFIX: Final[str] = field(default=os.environ.get("COMMAND_PREFIX", "!COMMAND_PREFIX"))
