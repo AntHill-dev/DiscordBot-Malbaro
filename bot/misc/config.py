7bd26708e31c2366c9872d4e2882581c978d6a88
Ivan Kovach <eeiprex@gmail.com>
2023-10-02 22:28:46 +0300

    [feat#config] Update `config.py` add docs, vars
    Add "Config" variable as the main config.
    Also add "StartUpParameters" for param in "init" class BotMarlboro

--- bot/misc/config.py
+++ bot/misc/config.py
@@ -6,13 +6,30 @@ from bot.misc.types import IntentsType
 from bot.misc.utils import SingletonABC, get_default_msg_intents
 
 
-class DefaultIntents(SingletonABC):
+class DefaultIntents:
+    """Storage for the bot's intents."""
+
     INTENTS: Final[IntentsType] = field(default_factory=get_default_msg_intents)
 
 
 @dataclass(frozen=True)
-class BotConfig(SingletonABC):
+class BotConfig(metaclass=SingletonABC):
+    """Bot configuration storage class.
+
+    Attributes:
+        TOKEN - A secret token for bot.
+        INTENTS - A using intents for bot.
+        COMMAND_PREFIX - A bot's command prefix.
+    """
+
     TOKEN: str = APIKeyDiscordBot.TOKEN
     INTENTS: IntentsType = DefaultIntents.INTENTS
     COMMAND_PREFIX: str = CommandPrefixDiscordBot.COMMAND_PREFIX
 
+
+Config = BotConfig()
+
+StartUpParameters = {
+    "command_prefix": Config.COMMAND_PREFIX,
+    "intents": Config.INTENTS,
+}
