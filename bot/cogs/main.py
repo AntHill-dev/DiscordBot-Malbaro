646a182f0874ac73ba333fff093f7aabdbd4fbc5
Ivan Kovach <eeiprex@gmail.com>
2023-10-02 22:27:16 +0300

    [add:docs] docstring for `setup_cogs_for_user` func in "cogs/main.py"

--- bot/cogs/main.py
+++ bot/cogs/main.py
@@ -5,6 +5,11 @@ usr_command_list = [
 ]
 
 
-def setup_cogs_for_user(bot: BotType):
+def setup_cogs_for_user(bot: BotType) -> None:
+    """Registering user commands for a specific bot.
+
+    Args:
+        bot: Bot for which commands will be registered for the user
+    """
     for cog in usr_command_list:
-        bot.load_extension(f"user.{cog}")
+        bot.load_extension(f"bot.cogs.user.{cog}")
