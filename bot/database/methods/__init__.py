8fea8800fddedb2f12f12477c092f39926399a12
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2023-10-31 17:44:44 +0300

    [add#database] Method `GetUserInfo`

--- bot/database/methods/__init__.py
+++ bot/database/methods/__init__.py
@@ -1,5 +1,6 @@
 from .get_user_about import GetUserAbout
 from .get_user_id import GetUserID
+from .get_user_info import GetUserInfo
 from .set_user_about import SetUserAbout
 from .set_user_info import SetUserInfo
 
@@ -7,21 +8,25 @@ from .set_user_info import SetUserInfo
 class Methods(
     GetUserAbout,
     GetUserID,
+    GetUserInfo,
     SetUserAbout,
     SetUserInfo,
 ):
     """Database methods class.
 
     Methods:
-        get_user_id_by_username(self: "main.DatabaseMalboro", username: str)
+        get_user_id_by_username(self: "main.DatabaseMalboro", username: str) -> int
             Get user id by username.
 
-        get_user_about(self: "main.DatabaseMalboro", user_id: int)
+        get_user_about(self: "main.DatabaseMalboro", user_id: int) -> str
             Get user about by user id.
 
-        set_user_about(self: "main.DatabaseMalboro", user_id: int, about: str)
+        get_user_info(self: "main.DatabaseMalboro", user_id: int) -> list
+            Get user info by user id.
+
+        set_user_about(self: "main.DatabaseMalboro", user_id: int, about: str) -> None
             Set user about by user id.
 
-        set_user_info(self: "main.DatabaseMalboro", user_id: int, messages: int, voice_time: int)
+        set_user_info(self: "main.DatabaseMalboro", user_id: int, messages: int, voice_time: int) -> None
             Set user info by user id.
     """
---
+++ bot/database/methods/get_user_info.py
@@ -0,0 +1,16 @@
+from bot.database import main
+
+
+class GetUserInfo:
+    """Get user info by user id."""
+
+    def get_user_info(self: "main.DatabaseMalboro", user_id: int) -> list:
+        """Get user info.
+
+        Args:
+            user_id: User id
+
+        Returns:
+            list: User info
+        """
+        return self.execute("SELECT * FROM info WHERE ID = %s;", user_id)
