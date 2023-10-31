977e4cfd72a087b800abed5c877e86950192345a
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2023-10-31 17:35:38 +0300

    [add#database] Method `SetUserInfo`

--- bot/database/methods/__init__.py
+++ bot/database/methods/__init__.py
@@ -1,12 +1,17 @@
 from .get_user_id import GetUserID
+from .set_user_info import SetUserInfo
 
 
 class Methods(
     GetUserID,
+    SetUserInfo,
 ):
     """Database methods class.
 
     Methods:
         get_user_id_by_username(self: "main.DatabaseMalboro", username: str)
             Get user id by username.
+
+        set_user_info(self: "main.DatabaseMalboro", user_id: int, messages: int, voice_time: int)
+            Set user info by user id.
     """
---
+++ bot/database/methods/set_user_info.py
@@ -0,0 +1,34 @@
+import psycopg2.sql
+
+from bot.database import main
+
+
+class SetUserInfo:
+    """Set user info by user id."""
+
+    def set_user_info(
+            self: "main.DatabaseMalboro",
+            user_id: int,
+            messages: int,
+            voice_time: int,
+    ) -> None:
+        """Set user info.
+
+        Args:
+            user_id: User id
+            messages: Messages count
+            voice_time: Voice time
+
+        Returns:
+            None
+        """
+        self.execute(
+            psycopg2.sql.SQL(
+                """
+                INSERT INTO info (id, messages, voice_time)
+                VALUES (%s, %s, %s)
+                ON CONFLICT (id) DO UPDATE
+                SET messages = excluded.messages, voice_time = excluded.voice_time
+                """,
+            ), user_id, messages, voice_time, noreturn=True,
+        )
